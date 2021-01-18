3026d93e2c59f087c623b0adca75116c9162226e
Viktoria <vikaarestova01@gmail.com>
2021-01-18 20:44:06 +0700

    Init CS

---
+++ OOP_laba8_cs/Icecream.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_laba8_cs
+{
+    class Icecream
+    {
+        private String name;
+        private String country;
+        private int massa;
+        private int milk;
+        private int expiration_date;
+        private int chocolate;
+        private Date date_post;
+        public String Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public String Country
+        {
+            get
+            {
+                return country;
+            }
+            set
+            {
+                country = value;
+            }
+        }
+
+        public int Massa
+        {
+            get
+            {
+                return massa;
+            }
+            set
+            {
+                massa = value;
+            }
+        }
+
+        public int Milk
+        {
+            get
+            {
+                return milk;
+            }
+            set
+            {
+                milk = value;
+            }
+        }
+        public int Expiration_date
+        {
+            get
+            {
+                return expiration_date;
+            }
+            set
+            {
+                expiration_date = value;
+            }
+        }
+        public int Chocolate
+        {
+            get
+            {
+                return chocolate;
+            }
+            set
+            {
+                chocolate = value;
+            }
+        }
+        public int Number
+        {
+            get
+            {
+                return date_post.Day;
+            }
+            set
+            {
+                date_post.Day = value;
+            }
+        }
+
+        public Icecream()
+        {
+            this.date_post = new Date();
+        }
+        public String Read_String()
+        {
+            String str = Console.ReadLine();
+            return str;
+        }
+        public int Read_Int()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            return n;
+        }
+        public void Init(String nazv, int mas, int sod_milk, int ex_date, int ch, String count, int x, int y, int k)
+        {
+            name = nazv;
+            massa = mas;
+            milk = sod_milk;
+            expiration_date = ex_date;
+            chocolate = ch;
+            date_post.Init(x, y, k);
+            country = count;
+        }
+        public void Display()
+        {
+            Console.WriteLine($"Name: {name} ");
+            Console.WriteLine($"Massa: {massa} ");
+            Console.WriteLine($"milk: {milk} ");
+            Console.WriteLine($"expiration_date: {expiration_date} ");
+            Console.WriteLine($"chocolate: {chocolate} ");
+            date_post.Display();
+            Console.WriteLine($"Country: {country} ");
+        }
+        public int Add(Icecream ice1, Icecream ice2)
+        {
+            Icecream ice3 = new Icecream();
+            ice3.massa = ice1.massa + ice2.massa;
+            return ice3.massa;
+        }
+        public void output_date_postavk()
+        {
+            date_post.output_date_postavk();
+        }
+        public void Country_output()
+        {
+            Console.WriteLine($"Country: {country}");
+        }
+        public int one(Icecream icecream)
+        {
+            return icecream.massa + 100;
+        }
+        public int two()
+        {
+            return one(this);
+        }
+        //перегрузка оператора + с помощью свойства Massa
+        public static Icecream operator +(Icecream ice1, Icecream ice2)
+        {
+            return new Icecream { Massa = ice1.massa + ice2.massa };
+        }
+
+        //перегрузка оператора ++ с помощью свойства Massa
+        public static Icecream operator ++(Icecream ice1)
+        {
+            return new Icecream { Massa = ice1.massa + 1 };
+        }
+    }
+}
---
+++ OOP_laba8_cs/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_laba8_cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Icecream ice = new Icecream();
+            String str_1 = "Lakomka";
+            String str_2 = "Russia";
+            int m = 250;
+            int mil = 100;
+            int ex = 60;
+            int ch = 1;
+            int x = 12;
+            int y = 10;
+            int k = 2020;
+
+            //жемонстрация работы со свойствами
+            ice.Name = "Lakomka";
+            ice.Country = "Russia";
+            Console.WriteLine($"Name: {ice.Name}");
+            ice.Number = 35;
+            Console.WriteLine($"Day: {ice.Number}");
+            ice.Number = 12;
+            Console.WriteLine($"Day: {ice.Number} \n");
+
+            //демонстрация работы с массивом любых объектов
+            object[] myArrayObject = new object[2];
+            myArrayObject[0] = "This is C Sharp";
+            myArrayObject[1] = 7;
+
+            //демонстрация работы с массивом класса Icecream
+            Icecream[] myIcecreamsObject = new Icecream[2];
+            myIcecreamsObject[0] = new Icecream();
+            myIcecreamsObject[0].Init(str_1, m, mil, ex, ch, str_2, x, y, k);
+            myIcecreamsObject[0].Display();
+
+            //демонстрация возврата значения через параметры out и ref
+            int f1 = 7;
+            int s;
+
+            IncrementRef(ref f1);
+            Sum(f1, 5, out s);
+            Console.WriteLine($"f1 + f2 = {s}");
+
+            //демоснтрация использования this
+            ice.Init(str_1, m, mil, ex, ch, str_2, x, y, k);
+            Console.WriteLine(ice.two());
+
+            //демонстрация перегрузки операторов + ++
+            Icecream ice_2 = new Icecream();
+            str_1 = "Fixiki";
+            str_2 = "Russia";
+            m = 120;
+            mil = 90;
+            ex = 75;
+            ch = 0;
+            x = 31;
+            y = 12;
+            k = 2020;
+            ice_2.Init(str_1, m, mil, ex, ch, str_2, x, y, k);
+            Icecream ice_3 = new Icecream();
+            ice_3 = ice + ice_2;
+            Console.WriteLine($"Сумма двух объектов класса Icecream = {ice_3.Massa}");
+            Console.WriteLine($"Префиксная инкрементация: {(++ice).Massa}");
+            Console.WriteLine($"Постфиксная инкрементация: {(ice++).Massa}");
+
+            
+            Console.ReadKey();
+        }
+
+        public static void Sum(int f1, int v, out int s)
+        {
+            s = f1 + v;
+        }
+
+        public static void IncrementRef(ref int f1)
+        {
+            f1++;
+            Console.WriteLine($"New argument: {f1}");
+        }
+    }
+}
